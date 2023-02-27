31e14ea3d3abe3962bd84521e0f42ec27c701c1e
diego <diegofernandoruizrojas@gmail.com>
2023-02-27 15:02:45 -0500

    innitial commit

---
+++ src/main/java/eci/data/edu/labadapostgres/LabadapostgresApplication.java
@@ -0,0 +1,13 @@
+package eci.data.edu.labadapostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabadapostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabadapostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/data/edu/labadapostgres/LabadapostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.data.edu.labadapostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabadapostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
